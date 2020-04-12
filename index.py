c9407dbe177856342d4a3d9be887138692776b71
Luis Sanchez <lsjaquez22@gmail.com>
2020-04-12 15:16:22 -0600

    recognize != token

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 def main():
 
-    transition_table = [{"letra": 1, "digit": 2, " ": 3, "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3},
+    transition_table = [{"letra": 1, "digit": 2, " ": 3, "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3, "!": 3},
                         {"letra": 1, "digit": 4, " ": 3,
-                            "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3},
+                            "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3, "!": 3},
                         {"letra": 4, "digit": 2, " ": 3,
-                            "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3},
+                            "\n": 3, ";": 3, ",": 3, "+": 3, "-": 3, "*": 3, "/": 3, "<": 3, ">": 3, "=": 3, "(": 3, ")": 3, "{": 3, "}": 3, "[": 3, "]": 3, "!": 3},
+                        {},
                         {},
                         {}]
     list_tokens = []
@@ -15,6 +16,7 @@ def main():
     state = 0
     index = 0
     token = ""
+    delimitador = ""
     error_token = ""
     while index < len(text):
         if state != 3 and state != 4:
@@ -36,11 +38,11 @@ def main():
                 print("STATE = 0")
                 list_tokens.append(token)
             delimitador = text[index]
-            if delimitador != " " and delimitador != "\n":
+            if delimitador != " " and delimitador != "\n" and delimitador != "!":
                 if delimitador == "<" and text[index + 1] == "=":
                     delimitador += "="
                     index += 1
-                if delimitador == "<" and text[index + 1] == "=":
+                if delimitador == ">" and text[index + 1] == "=":
                     delimitador += "="
                     index += 1
                 if delimitador == "=" and text[index + 1] == "=":
@@ -50,6 +52,13 @@ def main():
                 list_tokens.append(delimitador)
             token = ""
             state = 0
+            if delimitador == "!" and text[index + 1] == "=":
+                delimitador += "="
+                index += 1
+                print("DELIMITER ", state, " - ", delimitador)
+                list_tokens.append(delimitador)
+            elif delimitador == "!" and delimitador != "=":
+                state = 5
 
         if state == 4:
             error_token += token
@@ -57,7 +66,14 @@ def main():
                 error_token += text[index+1]
                 index += 1
             print("------- Error ------- Invalid Token", error_token)
-            index = len(text)+1
+            index = len(text) + 1
+        if state == 5:
+            delimitador = text[index]
+            while text[index+1] != " " and text[index+1] != "\n":
+                delimitador += text[index+1]
+                index += 1
+            print("------- Error ------- Delimitador Invalido", delimitador)
+            index = len(text) + 1
         print("----------", index)
         index += 1
     print(list_tokens)
